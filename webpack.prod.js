a37530d21a2253a9e013f74db3ae086e37714251
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2025-01-30 23:36:09 +0700

    Initialize

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },      
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
