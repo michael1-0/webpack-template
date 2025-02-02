85114447761617258007df679ed31bc47aff8d1f
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2025-02-02 15:27:24 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,10 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: "eval-source-map",
   devServer: {
     static: './dist',
+    watchFiles: ["./src/template.html"],
   },
   module: {
     rules: [
